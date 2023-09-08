b5908e3853adf6cbfefea56b7877e15b5811651d
= <jonmorri@msn.com>
2023-09-07 19:27:22 -0700

    add rollover image effects

--- app.py
+++ app.py
@@ -5,36 +5,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    images = []
-
-    for f in os.listdir('static/images'):
-        if f.endswith('.jpg'):
-            images.append('/static/images/' + f) 
-            
-    return render_template('index.html', images=images)
+    return render_template('index.html')
 
 @app.route('/about')
-@app.route('/about.html')
+#@app.route('/about.html')
 def about():
     return render_template('about.html')
 
 @app.route('/resume')
-@app.route('/resume.html')
+#@app.route('/resume.html')
 def resume():
     return render_template('resume.html')
 
 @app.route('/projects')
-@app.route('/projects.html')
+#@app.route('/projects.html')
 def projects():
     return render_template('projects.html')
 
 @app.route('/blog')
-@app.route('/blog.html')
+#@app.route('/blog.html')
 def blog():  # Renamed this function
     return render_template('blog.html')
 
 @app.route('/contact')
-@app.route('/contact.html')
+#@app.route('/contact.html')
 def contact():  # Renamed this function
     return render_template('contact.html')
 
